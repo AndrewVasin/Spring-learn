c10089fc16093102e836ae2726680f33b3b9ad1a
AndrewVasin <ironfeyd@gmail.com>
2022-05-20 20:04:41 +0300

    Spring learn, lesson 12

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -2,18 +2,35 @@ package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
 
 @Component
 public class MusicPlayer {
+    @Value("${musicPlayer.name}")
+    private String name;
+
+    @Value("${musicPlayer.volume}")
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
     private Music music1;
     private Music music2;
     private Music music3;
 
     @Autowired
-    public MusicPlayer(@Qualifier("rockMusic") Music music1, @Qualifier("classicalMusic") Music music2, @Qualifier("folkMusic") Music music3) {
+    public MusicPlayer(@Qualifier("rockMusic") Music music1,
+                       @Qualifier("classicalMusic") Music music2,
+                       @Qualifier("folkMusic") Music music3) {
         this.music1 = music1;
         this.music2 = music2;
         this.music3 = music3;
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -9,9 +9,14 @@ public class TestSpring {
           "applicationContext.xml"
         );
 
-        Computer computer = context.getBean("computer", Computer.class);
+//        Computer computer = context.getBean("computer", Computer.class);
+//
+//        System.out.println(computer);
 
-        System.out.println(computer);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
 
         context.close();
     }
